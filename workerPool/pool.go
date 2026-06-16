19984a5758d5afa045991f62510aea67d38c0bca
MaksimHank <orthopedicsalon5@gmail.com>
2026-06-16 23:13:05 +0300

    pool.go is done. Started worker.go

--- workerPool/pool.go
+++ workerPool/pool.go
@@ -6,8 +6,24 @@ import (
 )
 
 type Pool struct {
-	tasks  chan func() error
-	ctx    context.Context
-	cancel context.CancelFunc
-	wg     sync.WaitGroup
+	tasks      chan func() error
+	NumWorkers int
+	ctx        context.Context
+	cancel     context.CancelFunc
+	wg         sync.WaitGroup
+}
+
+func NewPool(numWorkers int) *Pool {
+	tasks := make(chan func() error, numWorkers)
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	wg := sync.WaitGroup{}
+
+	return &Pool{
+		tasks:      tasks,
+		NumWorkers: numWorkers,
+		cancel:     cancel,
+		ctx:        ctx,
+		wg:         wg,
+	}
 }
--- workerPool/worker.go
+++ workerPool/worker.go
@@ -1,7 +1,12 @@
 package workerPool
 
-func worker(in <-chan int, out chan<- int) {
-	for n := range in {
-		out <- n
+func worker(id int, tasks <-chan func() error, ctx context.Context, wg *sync.WaitGroup) {
+	for {
+		select {
+		case <-tasks:
+		case <-ctx.Done():
+			wg.Done()
+			return
+		}
 	}
 }
